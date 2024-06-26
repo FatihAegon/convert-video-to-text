cb30ea9b5e5ed767d4d70fcad6e73c51895b6d94
Oğuzhan Erenbağ <erenbagoguzhan@gmail.com>
2024-06-26 18:27:27 +0300

    audiototext.py

--- audiototext.py
+++ audiototext.py
@@ -1,18 +1,14 @@
 import speech_recognition as sr
 
-# Ses dosyasının yolu
 ses_path = r"C:\Users\erenb\ses\audio.wav"
 
-# Tanıma motoru oluşturma
 r = sr.Recognizer()
 
-# Ses dosyasını yükle
 with sr.AudioFile(ses_path) as source:
-    audio_data = r.record(source)  # Ses dosyasını oku
-
-    # Tanıma işlemi
+    audio_data = r.record(source) 
+    
     try:
-        text = r.recognize_sphinx(audio_data)  # CMU Sphinx ile tanıma
+        text = r.recognize_sphinx(audio_data)  
         print("Tanıma başarılı:", text)
     except sr.UnknownValueError:
         print("Tanınabilir bir şey bulunamadı.")
